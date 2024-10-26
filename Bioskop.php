761c320bda3e73359c2773ffcba97c0cd9335dba
radityadias <radityadias24@gmail.com>
2024-10-26 15:53:01 +0700

    doing few changes

--- Bioskop.php
+++ Bioskop.php
@@ -1,51 +1,58 @@
 <?php 
-    echo "SELAMAT DATANG DI BIOSKOP MASA KINI!!!\n";
-    echo "========================================\n";
-    echo "Pemesanan Tiket:\n";
-    echo "1. Dewasa (Rp60.000 akhir pekan, Rp50.000 hari biasa) \n2. Anak-anak (Rp40.000 akhir pekan, Rp30.000 hari biasa)\n";
-    echo "Pilih menu: ";
-    $menu = (int) readline();
-    // Validasi Menu
-    if($menu !== 1 && $menu !== 2){
-        echo "Menu yang anda masukkan tidak valid!";
-        exit();
-    }
+    do{
+        echo "\n\nSELAMAT DATANG DI BIOSKOP MASA KINI!!!\n";
+        echo "========================================\n";
+        echo "Pemesanan Tiket:\n";
+        echo "1. Dewasa (Rp60.000 akhir pekan, Rp50.000 hari biasa) \n2. Anak-anak (Rp40.000 akhir pekan, Rp30.000 hari biasa)\n";
+        $menu = (int) readline("Pilih menu: ");
+        // Validasi Menu
+        if($menu !== 1 && $menu !== 2){
+            echo "------------------------------------\n";
+            echo "Menu yang anda masukkan tidak valid!";
+            exit();
+        }
 
-    echo "Jumlah tiket: ";
-    // Validasi Tiket
-    $tiket = (int) readline();
-    if($tiket <= 0){
-        echo "Tiket yang anda masukkan tidak valid!";
-        exit();
-    }
-    echo "Hari Pemesanan (contoh: senin, selasa, dst.): ";
-    $hari = strtolower(readline());  
+        // Validasi Tiket
+        $tiket = (int) readline("Jumlah tiket: ");
+        if($tiket <= 0){
+            echo "------------------------------------\n";
+            echo "Tiket yang anda masukkan tidak valid!";
+            exit();
+        }
+        $hari = strtolower(readline("Hari Pemesanan (contoh: senin, selasa, dst.): "));  
 
-    // Validasi Hari
-    if (!isValidDay($hari)) {
-        echo "Hari yang dimasukkan tidak valid. Masukkan hari yang benar.\n";
-        exit();
-    } elseif ($menu !== 1 && $menu !== 2) {
-        echo "Menu yang dipilih tidak valid. Pilih 1 atau 2.\n";
-    } else {
-        // Menghitung total harga
-        $totalHarga = calculate($menu, $tiket, $hari);
-        
-        //Memberikan diskon 10%
-        if ($totalHarga > 150000) {
-            $discount = $totalHarga * 0.10;
-            $totalHarga -= $discount;
-            echo "Anda mendapatkan diskon 10%!\n";
-            echo "Harga setelah diskon: Rp" . $totalHarga . "\n";
+        // Validasi Hari
+        if (!isValidDay($hari)) {
+            echo "------------------------------------\n";
+            echo "Hari yang dimasukkan tidak valid. Masukkan hari yang benar.\n";
+            exit();
         } else {
-            echo "Total harga: Rp" . $totalHarga . "\n";
-        }
+            // Menghitung total harga
+            $totalHarga = calculate($menu, $tiket, $hari);
+            
+            //Memberikan diskon 10%
+            if ($totalHarga > 150000) {
+                $discount = $totalHarga * 0.10;
+                $totalHarga -= $discount;
+                echo "Anda mendapatkan diskon 10%!\n";
+                echo "------------------------------------\n";
+                echo "Harga setelah diskon: Rp" . $totalHarga . "\n";
+                echo "------------------------------------\n";
+            } else {
+                echo "------------------------------------\n";
+                echo "Total harga: Rp" . $totalHarga . "\n";
+                echo "------------------------------------\n";
+            }
 
-        if ($menu === 2) {
-            echo "Jumlah tiket anak-anak: " . $tiket . "\n";
-        }
-    }
+            if ($menu === 2) {
+                echo "Jumlah tiket anak-anak: " . $tiket . "\n";
+            }
 
+            echo "Apakah Anda ingin memesan lagi? (ketik 'ya' untuk memesan lagi, atau 'tidak' untuk keluar): ";
+            $pesanLagi = strtolower(readline());
+        }
+    }while($pesanLagi === "ya");
+    
     // Fungsi untuk memvalidasi hari
     function isValidDay($hari) {
         $validDays = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"];
